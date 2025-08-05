f5f0384b56ad413cf99f13f173b5893fd3222f09
missiamenou <komimissiamenou97@gmail/com>
2025-08-05 18:44:48 +0000

    Ajout de nouveaux dossiers pour l'interface Flutter

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color.fromRGBO(195, 58, 69, 1); 
+  static const Color secondary = Color(0xFFAE9051); 
+  static const Color background = Color(0xFF888888); 
+  static const Color textDark = Color(0xFF333333);  
+
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+
+  
+}
