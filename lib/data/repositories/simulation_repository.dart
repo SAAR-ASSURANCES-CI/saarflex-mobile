8a6249d2d9da47fcdf9e76d1cb5224f1797a5ea9
missiamenou <komimissiamenou97@gmail.com>
2025-10-15 10:09:31 +0000

    Remove unused logger import from simulation_repository.dart to improve code clarity and maintainability.

--- lib/data/repositories/simulation_repository.dart
+++ lib/data/repositories/simulation_repository.dart
@@ -1,7 +1,6 @@
 import 'package:saarflex_app/data/models/simulation_model.dart';
 import 'package:saarflex_app/data/models/critere_tarification_model.dart';
 import 'package:saarflex_app/data/services/simulation_service.dart';
-import 'package:saarflex_app/core/utils/logger.dart';
 
 class SimulationRepository {
   final SimulationService _simulationService;
