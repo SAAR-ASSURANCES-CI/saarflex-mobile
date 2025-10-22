d17dfedd54806a83286fc2b3bc80ffb5d691bcba
missiamenou <komimissiamenou97gmail.com>
2025-10-22 13:07:54 +0000

    changement de ip pour faire fonctionner l'app sur un emulateur android samsung A32

--- lib/core/utils/api_config.dart
+++ lib/core/utils/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://localhost:3000';
+  static const String baseUrl = 'http://192.168.2.2:3000';
   static const Duration timeout = Duration(seconds: 30);
 
   static const Map<String, String> defaultHeaders = {
