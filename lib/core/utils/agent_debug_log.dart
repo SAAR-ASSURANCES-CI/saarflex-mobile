0052de3fcccac82306825232e01be1ebd0f70fdd
missiamenou <140099439+missiamenou@users.noreply.github.com>
2026-01-20 09:24:06 +0000

    Clean up comments in AgentDebugLog class
    Removed debug logging comments indicating that logging is disabled for production.

--- lib/core/utils/agent_debug_log.dart
+++ lib/core/utils/agent_debug_log.dart
@@ -1,5 +1,3 @@
-/// Debug logger (disabled for production)
-/// This class is kept for API compatibility but logging is disabled
 class AgentDebugLog {
 
   static Future<void> log({
@@ -9,8 +7,6 @@ class AgentDebugLog {
     Map<String, Object?> data = const {},
     String runId = 'pre-fix',
   }) async {
-    // Debug logging disabled for production
-    // This method is kept for API compatibility but does nothing
   }
 }
 
